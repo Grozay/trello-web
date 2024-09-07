ef5d30f59336364b746ba9ed6bf717a242856538
quyendev <quyencutety@gmail.com>
2024-09-08 01:10:09 +0700

    addCssbaseline

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -22,6 +22,13 @@ module.exports = {
     'react/prop-types': 0,
     'react/display-name': 0,
 
+    'no-restricted-imports': [
+      'error',
+      {
+        'patterns': ['@mui/*/*/*']
+      }
+    ],
+
     'no-console': 1,
     'no-lonely-if': 1,
     'no-unused-vars': 1,
@@ -40,5 +47,6 @@ module.exports = {
     'comma-dangle': 1,
     'comma-spacing': 1,
     'arrow-spacing': 1
+
   }
 }
