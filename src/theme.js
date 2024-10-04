82a43f3a23be4cd7ffe08b225f4aba5146c0c19a
Quyendev <146425082+Grozay@users.noreply.github.com>
2024-10-04 10:56:15 +0700

    create app bar and no responsive

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,4 @@
+import { cyan, deepOrange, orange, teal } from '@mui/material/colors'
 import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 // import { red } from '@mui/material/colors'
 
@@ -5,26 +6,73 @@ import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
 // Create a theme instance.
 const theme = extendTheme({
   trello: {
-    appBarHeight: '48px',
-    boardBarHeight: '58px'
+    appBarHeight: '58px',
+    boardBarHeight: '60px'
   },
   colorSchemes: {
     light: {
       palette: {
-        gradient:
-          'linear-gradient(to left, var(--mui-palette-primary-main), var(--mui-palette-primary-dark))',
-        border: {
-          subtle: 'var(--mui-palette-neutral-200)'
-        }
+        primary: teal,
+        secondary: deepOrange
       }
     },
     dark: {
       palette: {
-        gradient:
-          'linear-gradient(to left, var(--mui-palette-primary-light), var(--mui-palette-primary-main))',
-        border: {
-          subtle: 'var(--mui-palette-neutral-600)'
+        primary: cyan,
+        secondary: orange
+      }
+    }
+  },
+  components: {
+    MuiCssBaseline: {
+      styleOverrides: {
+        body: {
+          '*::-webkit-scrollbar': {
+            width: '8px',
+            height: '8px'
+          },
+          '*::-webkit-scrollbar-thumb': {
+            background: '#bdc3c7',
+            borderRadius: '8px'
+          },
+          '*::-webkit-scrollbar-thumb:hover': {
+            background: '#00b894'
+          }
         }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none'
+        }
+      }
+    },
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({ theme }) => (
+          {
+            color: theme.palette.primary.main,
+            fontSize: '0.875rem',
+            '& .MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.primary.light
+            },
+            '&:hover .MuiOutlinedInput-notchedOutline': {
+              borderColor: theme.palette.primary.main
+            },
+            '& fieldset': {
+              borderWidth: '1px !important  '
+            }
+          }
+        )
+      }
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({ theme }) => ({
+          color: theme.palette.primary.main,
+          fontSize: '0.875rem'
+        })
       }
     }
   }
